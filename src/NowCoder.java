5ae2cacba5f8b881d3f6e0ddac93b574c568d774
viktorxhzj <harryxh@live.com>
2020-03-25 15:10:34 +0800

    0325

--- src/NowCoder.java
+++ src/NowCoder.java
@@ -1401,3 +1401,97 @@ class Solution45 {
     }
 
 }
+
+// 0325
+
+/*
+每年六一儿童节,牛客都会准备一些小礼物去看望孤儿院的小朋友,今年亦是如此。
+HF作为牛客的资深元老,自然也准备了一些小游戏。其中,有个游戏是这样的:
+首先,让小朋友们围成一个大圈。然后,他随机指定一个数m,让编号为0的小朋友开始报数。
+每次喊到m-1的那个小朋友要出列唱首歌,然后可以在礼品箱中任意的挑选礼物,
+并且不再回到圈中,从他的下一个小朋友开始,继续0...m-1报数....这样下去....
+直到剩下最后一个小朋友,可以不用表演,并且拿到牛客名贵的“名侦探柯南”典藏版(名额有限哦!!^_^)。
+请你试着想下,哪个小朋友会得到这份礼品呢？(注：小朋友的编号是从0到n-1)
+ */
+class Solution46 {
+    public int LastRemaining_Solution(int n, int m) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        for (int i = 0; i < n; i ++) {
+            list.add(i);
+        }
+
+        int bt = 0;
+        while (list.size() > 1) {
+            bt = (bt + m - 1) % list.size();
+            list.remove(bt);
+        }
+
+        return list.size() == 1 ? list.get(0) : -1;
+    }
+
+}
+
+/*
+求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）
+ */
+class Solution47 {
+    public int Sum_Solution(int n) {
+        int sum = n;
+        boolean arg = (n > 0) && ((sum += Sum_Solution(n-1)) > 0);
+        return sum;
+    }
+}
+
+/*
+写一个函数，求两个整数之和，要求在函数体内不得使用+、-、*、/四则运算符号。
+ */
+class Solution48 {
+    public int Add(int num1,int num2) {
+        while( num2!=0 ) {
+            int sum = num1 ^ num2;
+            int carray = (num1 & num2) << 1;
+            num1 = sum;
+            num2 = carray;
+        }
+        return num1;
+    }
+}
+
+/*
+将一个字符串转换成一个整数，要求不能使用字符串转换整数的库函数。
+数值为0或者字符串不是一个合法的数值则返回0
+ */
+class Solution49 {
+    public int StrToInt(String str) {
+        int res = 0;
+        int length = str.length();
+        if (str == null || length == 0) return 0;
+        int i = 0;
+        char first = str.charAt(i);
+        boolean negative = false;
+        if (first == '-') {
+            negative = true;
+            i++;
+        }
+        else if (first == '+') i++;
+        int diff;
+        while (i < length) {
+            diff = str.charAt(i) - '0';
+            if (diff > 9 || diff < 0) return 0;
+            if (!negative) {
+                if (res > Integer.MAX_VALUE / 10) return 0;
+                res *= 10;
+                if (res > Integer.MAX_VALUE - diff) return 0;
+                res += diff;
+            }
+            else {
+                if (res < Integer.MIN_VALUE / 10) return 0;
+                res *= 10;
+                if (res < Integer.MIN_VALUE + diff) return 0;
+                res -= diff;
+            }
+            i++;
+        }
+        return res;
+    }
+}
