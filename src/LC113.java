d71bd65f3d1d4e6f76d861f38192a9e707133100
Hezijian Xiao <harryxh@live.com>
2020-04-23 18:28:54 +0800

    updated 0423

---
+++ src/LC113.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import datastructure.TreeNode;
+
+public class LC113 {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        LinkedList<Integer> arr = new LinkedList<>();
+        preOrder(arr, sum, root);
+        return res;
+    }
+
+    public void preOrder(LinkedList<Integer> list, int rest, TreeNode node) {
+        // 如果当前为空节点
+        if (node == null) return ;
+
+        rest -= node.val;
+        list.offerLast(node.val);
+
+        // 如果当前为叶节点且总和等于目标
+        if (node.left == null && node.right == null && rest == 0) {
+            res.add(new LinkedList<>(list));
+            list.pollLast();
+            return ;
+        }
+        preOrder(list, rest, node.left);
+        preOrder(list, rest, node.right);
+        list.pollLast();
+    }
+}
--- src/LC145.java
+++ src/LC145.java
@@ -1,1 +1,44 @@
-public class LC145 {}
+import datastructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class LC145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        // 用来存放结果
+        List<Integer> res = new ArrayList<>();
+        // 模拟系统的栈
+        Stack<TreeNode> stack = new Stack<>();
+        if (root == null) return res;
+        // 用来记录当前节点
+        TreeNode node = root;
+        // 用来记录上一个弹出节点
+        TreeNode pre = null;
+
+        while (true) {
+            // 如果有左节点就一直压入栈中
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            // 没有左节点压了，开始看向栈顶节点，判断栈是否为空
+            if (stack.isEmpty()) break;
+            node = stack.peek();
+            // 如果栈顶节点没有右节点，则是叶节点，可以弹出；
+            // 如果栈顶有右节点，但是右节点是上一个弹出的节点，也可以弹出。
+            if (node.right == null || pre == node.right) {
+                // 弹出节点，打印值
+                node = stack.pop();
+                res.add(node.val);
+                // 更新上个弹出的节点
+                pre = node;
+                // 更新当前节点为null，防止再次压入栈中
+                node = null;
+            }
+            // 如果有右节点且右节点没有被遍历过，向右节点前进
+            else node = node.right;
+        }
+        return res;
+    }
+}
